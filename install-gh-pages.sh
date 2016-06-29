4b014bcfc0a18e29cfeac9c462c9da7ec595962c
john.d.martin.iii <john.d.martin.iii@gmail.com>
2016-06-29 17:10:33 -0400

    update

--- install-gh-pages.sh
+++ install-gh-pages.sh
@@ -3,13 +3,17 @@
 #RUN WITH SUDO
 
 # update packages
-apt-get update
+apt-get update -y
 
-apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+
+# Install build tools
+
+apt-get install -y build-essentials
 
 # install ruby
 
-apt-get install ruby2.0 ruby2.0-dev
+apt-get install -y ruby2.0 ruby2.0-dev
 
 # set gem to link to gem2.0
 
@@ -17,10 +21,14 @@ ln -sfn /usr/bin/ruby2.0 /usr/bin/ruby
 ln -sfn /usr/bin/gem2.0 /usr/bin/gem 
 ln -sfn /usr/bin/rake2.0 /usr/bin/rake 
 
+# Fix configuration
+
+echo 
+
 # update gems
 
-gem2.0 update --no-ri --no-rdoc 
+gem2.0 update --no-document
 
 # install github-pages
 
-gem2.0 install github-pages --no-ri --no-rdoc
+gem2.0 install github-pages --no-document 
